a5a573a848df73673788408ca8d05df5facaf802
vincentmhhon <vincentmhhon@gmail.com>
2017-11-15 10:37:33 +0800

    feat: Completed Create Card

--- components/CreateCard.js
+++ components/CreateCard.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { View, Text, TextInput } from 'react-native'
-import { connect } from 'react-redux' 
+import { connect } from 'react-redux'
 import { NavigationActions } from 'react-navigation'
 import { createCard } from '../actions'
 import  Button from './Button'
